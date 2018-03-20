dff94af6474968246c6dead23eb0bf7c8a20ee69
JordanTFA <Jordantfa@hotmail.co.uk>
2018-03-20 08:39:50 +0000

    Added Node class

---
+++ src/Node.java
@@ -0,0 +1,13 @@
+import java.awt.Color;
+
+public class Node {
+	
+	public String name;
+	public Color colour;
+
+	public Node(String name, Color colour) {
+		this.name = name;
+		this.colour = colour;
+	}
+
+}
