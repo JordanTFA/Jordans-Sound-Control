63e46603800d2e32718e099c1348457641a1e7d5
JordanTFA <Jordantfa@hotmail.co.uk>
2018-09-07 23:48:44 +0100

    More track selection

--- src/AudioTracks.java
+++ src/AudioTracks.java
@@ -2,7 +2,10 @@ import java.io.File;
 import java.util.ArrayList;
 
 import javafx.scene.Scene;
+import javafx.scene.control.ComboBox;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
 
 public class AudioTracks {
@@ -13,16 +16,46 @@ public class AudioTracks {
 		stage.setTitle("Select a track");
 		
 		VBox layout = new VBox(20);
-		Scene scene = new Scene(layout, 300, 300);
 		
 		ArrayList<String> tracks = getTracks();
+		ComboBox<String> combo = fillOutTracks(tracks);
+
+		layout.getChildren().addAll(drawCircles(nodes), combo);
 		
+		Scene scene = new Scene(layout, 300, 300);
 		stage.setScene(scene);
 		stage.showAndWait();
 	}
 	
+	public static ComboBox<String> fillOutTracks(ArrayList<String> tracks){
+		
+		ComboBox<String> combo = new ComboBox<String>();
+		
+		combo.setPromptText("Select a Track:");
+		
+		for(String track : tracks){
+			combo.getItems().add(track);
+		}
+		return combo;
+	}
+	
+	public static HBox drawCircles(ArrayList<Node> nodes){
+		
+		HBox circleLayout = new HBox(5);
+		
+		double radius = 15;
+		
+		for(Node node : nodes){
+			Circle circle = new Circle(15, node.colour);
+			circleLayout.getChildren().add(circle);
+		}
+		
+		return circleLayout;	
+	}
+	
 	public static ArrayList<String> getTracks(){
 		
+		ArrayList<String> tracks = new ArrayList<String>();
 		String path = "res//tracks//";
 		
 		File folder = new File(path);
@@ -30,9 +63,8 @@ public class AudioTracks {
 		
 		for(File file : listOfFiles){
 			System.out.println(file.getName());
+			tracks.add(file.getName());
 		}
-		
-		// TODO: Get list of all files in the folder
-		return null;
+		return tracks;
 	}
 }
